143886024c0c504569a7f5fc6cb1d3a41ee4a783
Katios <kate.chonkina@gmail.com>
2021-01-16 14:33:21 +0600

    Initial Commit

---
+++ ApiAppCombine/ApiAppCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiAppCombineApp.swift
+//  ApiAppCombine
+//
+//  Created by Екатерина Чернова on 16.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct ApiAppCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
